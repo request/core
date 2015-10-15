16d8711f4cbe0315a3ebe4a7a90f4b838ee6b52e
simov <simeonvelichkov@gmail.com>
2015-10-15 10:53:30 +0300

    Move request's options initialization into separate module

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
 var util = require('util')
 var config = require('./lib/config')
-  , utils = require('./lib/utils')
-  , response = require('./lib/response')
-  , modules = require('./lib/modules')
+var _request = require('./lib/request')
 var HTTPDuplex = require('./lib/http-duplex')
 
 util.inherits(Request, HTTPDuplex)
@@ -13,108 +11,12 @@ function Request (protocol) {
   HTTPDuplex.call(this, protocol)
 }
 
-function request (_options) {
+function request (_options, init) {
   var options = config.init(_options)
   var req = new Request(options.protocol)
 
-  if (process.env.DEBUG) {
-    var log = require('@http/log')
-    log(req)
-  }
-
-  req.on('response', response(req, options))
-
-  if (options.redirect) {
-    var redirect = modules('redirect')
-    redirect(req, options)
-  }
-
-  if (options.gzip) {
-    var gzip = modules('gzip')
-    gzip(req, options)
-  }
-
-  if (options.multipart) {
-    var multipart = require('./lib/options/multipart')
-    multipart(req, options)
-  }
-
-  if (options.qs) {
-    var qs = require('./lib/options/qs')
-    qs(req, options)
-  }
-  if (options.form) {
-    var form = require('./lib/options/form')
-    form(req, options)
-  }
-  if (options.json) {
-    var json = require('./lib/options/json')
-    json(req, options)
-  }
-
-  if (options.cookie) {
-    var cookie = modules('cookie')
-    cookie(req, options)
-  }
-
-  if (options.callback) {
-    if (typeof options.callback === 'function') {
-      var callback = require('./lib/options/callback')
-      callback(req, options)
-    }
-    else {
-      throw new Error('calback should be a function')
-    }
-  }
-
-  req.once('init', init)
-  process.nextTick(init)
-
-  function init () {
-    if (req._initialized) return
-
-    if (options.length) {
-      var length = require('./lib/options/length')
-      length(req, options, function (length) {
-        if (length) {
-          options.headers.set('content-length', length)
-        }
-        _init()
-      })
-    }
-    else {
-      _init()
-    }
-
-    function _init () {
-      if (options.parse) {
-        var parse = require('./lib/options/parse')
-        parse(req, options)
-      }
-      if (options.body || req._src || !options.end) {
-        if (options.headers.get('content-length') === undefined) {
-          options.headers.set('transfer-encoding', 'chunked')
-        }
-      }
-
-      if (options.auth || options.oauth || options.hawk || options.httpSignature || options.aws) {
-        var auth = modules('auth')
-        auth(req, options)
-      }
-
-      req.emit('options', options)
-      req.init(utils.filter(options))
-
-      if (options.body) {
-        var body = require('./lib/options/body')
-        body(req, options)
-      }
-
-      if (options.end) {
-        var end = require('./lib/options/end')
-        end(req, options)
-      }
-    }
+  if (init === undefined) {
+    _request(req, options)
   }
 
   return req
@@ -122,3 +24,4 @@ function request (_options) {
 
 module.exports = request
 request.Request = Request
+request.init = _request
