4980c8a90a06fc53198d0f20d14525f6583c09e6
simov <simeonvelichkov@gmail.com>
2015-10-22 13:21:11 +0300

    The cookie option accepts tough-cookie jar object

--- test/13-cookie.js
+++ test/13-cookie.js
@@ -4,6 +4,7 @@ var fs = require('fs')
   , http = require('http')
 var should = require('should')
   , debug = require('debug')
+  , tough = require('tough-cookie')
 var request = require('@http/client')
 
 console.server = debug('server')
@@ -39,6 +40,46 @@ describe('- cookie', function () {
         cookie: true,
         callback: function (err, res, body) {
           body.should.equal('a=b; Domain=localhost; c=d')
+          done()
+        }
+      })
+    })
+
+    after(function (done) {
+      server.close(done)
+    })
+  })
+
+  describe('cookie jar', function () {
+    var server
+    before(function (done) {
+      server = http.createServer()
+      server.on('request', function (req, res) {
+        if (req.url === '/redirect') {
+          res.writeHead(301, {
+            location: '/',
+            'set-cookie': 'e=f;'
+          })
+          res.end()
+        }
+        else {
+          res.end(req.headers.cookie)
+        }
+      })
+      server.listen(6767, done)
+    })
+
+    it('1', function (done) {
+      var jar = new tough.CookieJar()
+      jar.setCookieSync('c=d', 'http://localhost:6767/redirect')
+
+      var req = request({
+        url: 'http://localhost:6767/redirect',
+        headers: {cookie: 'a=b; Domain=localhost'},
+        redirect: true,
+        cookie: jar,
+        callback: function (err, res, body) {
+          body.should.equal('a=b; Domain=localhost; c=d; e=f')
           done()
         }
       })
